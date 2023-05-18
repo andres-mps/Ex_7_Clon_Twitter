1ac4ee982a020cdff2ddffc2f3272c7afb14cdf7
valentinalr <valentina.lerose@outlook.com>
2023-05-18 18:31:13 -0300

    register

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pagesController = require("../controllers/pagesController");
+const registerController = require("../controllers/registerController");
 
 router.get("/", pagesController.showHome);
 router.get("/login", function (req, res) {
@@ -9,6 +10,7 @@ router.get("/login", function (req, res) {
 router.get("/register", function (req, res) {
   res.render("pages/register");
 });
+router.post("/register", registerController.createUser);
 router.get("*", function (req, res) {
   res.status(404).render("pages/404");
 });
