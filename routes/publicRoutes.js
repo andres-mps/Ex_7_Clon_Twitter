196de90b2e9e019ba14f14a2273ec26096e41c25
Lorena Rincon <lorec.rincon@gmail.com>
2023-05-18 20:11:10 -0300

    Profile view changes

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -9,6 +9,10 @@ router.get("/login", function (req, res) {
 router.get("/register", function (req, res) {
   res.render("pages/register");
 });
+//Luego se moverá esta ruta a authRoutes//
+router.get("/profile", function (req, res) {
+  res.render("pages/profile");
+});
 router.get("*", function (req, res) {
   res.status(404).render("pages/404");
 });
